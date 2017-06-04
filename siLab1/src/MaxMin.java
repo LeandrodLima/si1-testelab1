0d961861acae53cc37b67790101892d1641b9e05
Leandro <leandro@Metal>
2017-06-04 19:38:57 -0300

    algoritimo retorna agora o maximo valorde um array

--- siLab1/src/MaxMin.java
+++ siLab1/src/MaxMin.java
@@ -1,4 +1,17 @@
 
 public class MaxMin {
+	
+
+	public int  maximo(int[] array){
+		int max = 0;
+		for(int i = 0; i <= array.length; i++){
+			if(array[i]> max){
+				max = array[i];
+			}
+			
+		}
+		
+		return max;
+		
     
-}
+}}
